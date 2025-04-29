d3fb01b8930669aedb4e6545509b7ddc8cdb1b6d
Farahtamer16 <farahisthebest11@gmail.com>
2025-04-29 10:02:05 +0800

    Update build script to install Node.js and Firebase CLI

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 echo "Installing Python packages..."
 pip install -r requirements.txt
 
+echo "Installing Node.js..."
+curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+apt-get install -y nodejs
+
 echo "Installing Firebase CLI..."
 npm install -g firebase-tools
